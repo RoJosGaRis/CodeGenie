d447a570176befdc378468510caa3e57fce61221
RoJosGaRis <roberto.jgr03@gmail.com>
2023-09-23 21:21:05 -0600

    .

--- src/app/landing/page.js
+++ src/app/landing/page.js
@@ -1,13 +1,38 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import { useSession } from "next-auth/react";
 
 const IndexPage = () => {
   const { data: session, status } = useSession();
 
+  useEffect(() => {
+    if (session) {
+      const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(session),
+      };
+
+      fetch("http://localhost:4000/users/newUser", requestOptions)
+        .then((res) => res.json())
+        .then(
+          (result) => {
+            console.log(result);
+          },
+          (error) => {
+            console.error(error);
+          }
+        );
+    }
+  }, [session]);
+
   if (session) {
-    return <p>Hola {session.user.name}, bienvenido de vuelta</p>;
+    return (
+      <div>
+        <p>{JSON.stringify(session)}</p>;
+      </div>
+    );
   }
 };
 
