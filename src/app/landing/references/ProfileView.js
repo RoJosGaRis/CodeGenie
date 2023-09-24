f38e08dd616069166e6b4ce95d1696514ebb2c36
RoJosGaRis <roberto.jgr03@gmail.com>
2023-09-24 11:34:55 -0600

    final

--- src/app/landing/references/ProfileView.js
+++ src/app/landing/references/ProfileView.js
@@ -4,24 +4,30 @@ import styles from "./references.module.css";
 const ProfileView = ({ profile }) => {
   return (
     <div className={styles.profileContainer}>
-      <h2>Languages</h2>
-      <ul>
-        {profile.languages.map((language, index) => (
-          <li key={index}>{language}</li>
-        ))}
-      </ul>
-      <h2>Skills</h2>
-      <ul>
-        {profile.skills.map((skill, index) => (
-          <li key={index}>{skill}</li>
-        ))}
-      </ul>
-      <h2>Tools</h2>
-      <ul>
-        {profile.tools.map((tool, index) => (
-          <li key={index}>{tool}</li>
-        ))}
-      </ul>
+      <div>
+        <h2>Languages</h2>
+        <ul>
+          {profile.languages.map((language, index) => (
+            <li key={index}>{language}</li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <h2>Skills</h2>
+        <ul>
+          {profile.skills.map((skill, index) => (
+            <li key={index}>{skill}</li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <h2>Tools</h2>
+        <ul>
+          {profile.tools.map((tool, index) => (
+            <li key={index}>{tool}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
